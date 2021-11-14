0273ff0194aea79374161fabcdf0e309d42ed1b0
Graham Sutherland <gsuberland@gmail.com>
2021-11-14 00:21:48 +0000

    Getting ready for release

---
+++ build.py
@@ -0,0 +1,34 @@
+import io
+import re
+import hashlib
+from base64 import b64encode
+from os import listdir
+from os.path import join, isfile
+from shutil import copyfile
+from pprint import pprint
+
+fin = open("src/cpuid.html", mode="r", encoding="utf-8")
+contents = fin.read()
+fin.close()
+
+targetFiles = re.findall(r'data-build-sri="([^"]+)"', contents)
+pprint(targetFiles)
+
+for targetFile in targetFiles:
+    h = hashlib.sha256()
+    fhash = open(join("src/", targetFile), 'rb')
+    h.update(fhash.read())
+    fhash.close()
+    hash = b64encode(h.digest()).decode('utf-8')
+    print(hash)
+    contents = contents.replace(r'data-build-sri="' + targetFile + '"', 'integrity="sha256-' + hash + '"')
+
+fout = open("build/index.html", mode="w", encoding="utf-8")
+fout.write(contents)
+fout.close()
+
+files = [f for f in listdir("src/") if (isfile(join("src/", f)) and not f == "cpuid.html")]
+for file in files:
+    print("copying src/" + file + " to build/" + file)
+    copyfile("src/" + file, "build/" + file)
+print("done")
