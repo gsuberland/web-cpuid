1ca5fe632f84d23cb7f4eef790cc0e14cff35f34
Graham Sutherland <gsuberland@gmail.com>
2021-11-14 00:54:54 +0000

    Moved button event handlers to cpuid_startup.js

--- src/cpuid_startup.js
+++ src/cpuid_startup.js
@@ -1,2 +1,5 @@
 loadValues();
 fix_font_size();
+document.getElementById('button_loadValues').onclick = loadValues;
+document.getElementById('button_toggleBitRanges').onclick = toggleBitRanges;
+document.getElementById('button_toggleRegisterValues').onclick = toggleRegisterValues;
